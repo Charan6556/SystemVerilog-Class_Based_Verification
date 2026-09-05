98822b05c4aaa2f5a1e4f8c2505a4f7564d403fd
Charan Gundepinni <142422435+Charan6556@users.noreply.github.com>
2026-09-05 15:53:26 -0600

    Add 02_full_adder/environment.sv

---
+++ 02_full_adder/environment.sv
@@ -0,0 +1,38 @@
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+
+class environment;
+  generator gen;
+  driver drv;
+  monitor mon;
+  scoreboard scb;
+
+  mailbox #(transaction) gen2drv;
+  mailbox #(transaction) mon2scb;
+
+  virtual full_adder_if vif;
+
+  function new(virtual full_adder_if vif);
+    this.vif = vif;
+
+    gen2drv = new();
+    mon2scb = new();
+
+    gen = new(gen2drv);
+    drv = new(vif, gen2drv);
+    mon = new(vif, mon2scb);
+    scb = new(mon2scb);
+  endfunction
+
+  task run();
+    fork
+      gen.main();
+      drv.main();
+      mon.main();
+      scb.main();
+    join
+  endtask
+endclass
